10f9e83d6ced0cc8cc46d5a41e88c9f90e05f01c
Sonal2000 <63189305+Sonal2000@users.noreply.github.com>
2020-08-15 17:14:02 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,12 @@
+   function commercial(){
+       document.getElementById("services").innerHTML="Commercial";
+       document.getElementById("header2").innerHTML="Commercial"; 
+   }
+   function office(){
+    document.getElementById("services").innerHTML="Office";
+    document.getElementById("header2").innerHTML="Office"; 
+}
+function residential(){
+    document.getElementById("services").innerHTML="Residential";
+    document.getElementById("header2").innerHTML="Residential"; 
+}
